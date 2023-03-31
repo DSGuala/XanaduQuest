a225cfa70fd1bbd0520e097f76c404f8316ab7b1
Diego <diego_guala@hotmail.com>
2023-03-31 09:31:38 -0400

    Update, integration

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -1,19 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
 public class TimerScript : MonoBehaviour
 {
     public float TimeLeft;
     public bool TimerOn = false;
 
-    public TextMeshProUGUI TimerTxt;
+    public Text TimerTxt;
 
     // Start is called before the first frame update
     void Start()
     {
-        TimerOn = true; 
+        // TimerOn = true; 
     }
 
     // Update is called once per frame
@@ -30,6 +30,9 @@ public class TimerScript : MonoBehaviour
             {
                 TimeLeft = 0;
                 TimerOn = false;
+                // call the world manager to make a measurement
+                GameObject worldManager = GameObject.Find("World Manager");
+                // worldManager.GetComponent<WorldManager>()
             }
         }
     }
@@ -41,7 +44,7 @@ public class TimerScript : MonoBehaviour
         float minutes = Mathf.FloorToInt(currentTime / 60);
         float seconds = Mathf.FloorToInt(currentTime % 60);
 
-        TimerTxt.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+        TimerTxt.text = string.Format("Measure in {0:00} : {1:00}", minutes, seconds);
 
     }
 }
